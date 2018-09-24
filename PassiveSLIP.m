79d184c96f5afd84b4b27bfeffeb80ef57eb9e30
Roy Xing <00royxing@gmail.com>
2018-09-24 19:36:28 -0400

    Add files via upload

--- PassiveSLIP.m
+++ PassiveSLIP.m
@@ -1,37 +1,37 @@
-function dy = PassiveSLIP(h0, xVelStart, theta, dDef, k, m)
+function dy = PassiveSLIP(theta, dDef, k, m)
 % ATTEMPTSLIP attempt at a passive SLIP model by Roy X.
 % Find (x, y) of the point mass!
     
-    xtd, ytd = 0; % Both y and x touchdown points are x = 0
-    % Initial values of COM's x & y
+    % Do I need these here?
+    xtd = 0;
+    ytd = 0;
+    
+    % Initial values of COM's x & y, do I even need these given y0 is a
+    % thing?
     x = 0;
     y = h0;
     g = 9.8;
-    fig = figure;
-    ax = axes;
-    ax.XLim = [0 50];
-    ax.YLim = [0 40];
+    
+    % This is probably for flight phase and probably shouldn't be here with
+    % stance stuff
     h = h0; % h is equal to the highest value of y during the sim. Not sure the proper way to find that yet
     
     % Functions that describe the motion of the SLIP Model's COM
-    
+    d = sqrt((x - xtd)^2 + (y - ytd)^2);
+    sinT = y / d;
+    cosT = abs(xtd - x) / d;
     Fs = k * (dDef - d);
-    Fy = Fs * sin(theta); % How to express as m * y double dot?
-    Fx = Fs * cos(theta); % How to express as m * x double dot?
-    Fyt = Fs * sin(theta) - g;
-    %d = sqrt((m * g * h) / (0.5 * k));
-    d = y / sin(theta);
+    Fy = Fs * sinT;
+    Fx = Fs * cosT;
+    Fyt = Fs * sinT - g;
+    
     %d = d0 in flight phase, not sure how to impliment/express that just
     %yet
     
     
-    % TODO: figure out how to express the state vector here, how to
-    % write/express x, x dot, y, y dot
-    % what do to with the whole fact that the state vector as a derivative
-    % of time?
-    dy(1, 1) = y(2); % Named function y due to standard MATLAB Convention? Check if this is proper
-    dy(2, 1) = (); % How to write x double dot here?
+    dy(1, 1) = y(2); % Is this correct? Why is y(2) here like that and what's up with the inputs of dy
+    dy(2, 1) = (m / Fx);
     dy(3, 1) = y(4);
-    dy(4, 1) = ();
+    dy(4, 1) = (m / Fyt); % Would Fyt be correct here instead of Fy?
     
 
--- sim_PassiveSLIP.m
+++ sim_PassiveSLIP.m
@@ -1,19 +1,31 @@
 % Plot the trajectory of the point mass of the SLIP model
 clear all, close all, clc
 
-h0 = [0, 20]; % Height at which the SLIP model is dropped from
-xVelStart = [0, 2]; % Starting fwrd velocity of the mass
+% TODO: Double Check if your initial conditions and numbers make physical
+% sense
+
 theta = [0, 45];
 dDef = 0.2;
 k = 100;
 m = 10;
 
+y0 = [0, 5, 5, -9.8]; % Starting conditions of the state vector x, y, fwrd vel, upwrd vel
+
+refine = 4;
+options = odeset('Events', @events, 'OutputFcn', @odeplot, 'OutputSel', 1, ...
+    'Refine', refine);
+
+% graph stuff
+fig = figure;
+ax = axes;
+ax.XLim = [0 50];
+ax.YLim = [0 40];
 box on
 hold on;
 
 tspan = 0:.1:50; % How long the program runs, not sure if I need this
 
-[x, y] = ode45(@(x, y)PassiveSLIP(h0, xVelStart, theta, dDef, k, m), tspan, y0); % Need to figure out what the last two arguments do
+[t, y, te, ye, ie] = ode45(@(x, y)PassiveSLIP(theta, dDef, k, m), tspan, y0, options); % Need to figure out what the last two arguments do
 
 for i = 1 : 100
     % Plot the x and y coordinates of the SLIP model's COM
