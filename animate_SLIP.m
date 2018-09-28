e78d4d39c1b7f3832f2bffbbb2dc67b00dedaeac
Roy Xing <00royxing@gmail.com>
2018-09-28 15:49:02 -0700

    Add files via upload

---
+++ animate_SLIP.m
@@ -0,0 +1,8 @@
+function [] = animate_SLIP(q,s)
+%ANIMATE_SLIP animate the SLIP model
+%   Take in the state vector and forces to model the SLIP model
+
+    fig = figure(); % TODO: Figure out this whole animation process thing
+
+end
+
